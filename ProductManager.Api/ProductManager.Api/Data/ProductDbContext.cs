a3b1f3b0f797eba40609877f6d321aff7ba3d853
Sadat-09 <85846183+Sadat-09@users.noreply.github.com>
2025-11-14 23:42:20 +0600

    Initial commit with API + UI

---
+++ ProductManager.Api/ProductManager.Api/Data/ProductDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManager.Api.Models;
+
+namespace ProductManager.Api.Data
+{
+    public class ProductDbContext: DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ product-manager-ui/src/app/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import{ Product } from '../models/product.model';
+import{ HttpClient } from '@angular/common/http';
+import{ Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl =  'https://localhost:7123/api/products';
+
+  constructor(private http: HttpClient) { }
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  } 
+  getProduct(id: number): Observable<Product> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+  updateProduct(product: Product): Observable<void> {
+    const url = `${this.apiUrl}/${product.id}`;
+    return this.http.put<void>(url, product);
+  }
+  deleteProduct(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+
+}
